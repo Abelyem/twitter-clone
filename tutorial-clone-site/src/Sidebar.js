1ff60d0513686e379c3c789a87ce7838f896ec97
Abelyem <abel.yemane1@hotmail.co.uk>
2021-02-07 12:07:27 +0000

    initial commit

---
+++ tutorial-clone-site/src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Sidebar.css'
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+            {/* Twitter Icon */}
+            
+            {/* Sidebar option */}
+            {/* Sidebar option */}
+            {/* Sidebar option */}
+            {/* Sidebar option */}
+
+            {/* Button --> Tweet */}
+        </div>
+    );
+}
+
+export default Sidebar;
