4afe92ddbca3959367174a7a8511445dc7bdc0a0
Abelyem <abel.yemane1@hotmail.co.uk>
2021-02-16 22:27:31 +0000

    created all 3 separate parts of twitter page (sidebar, feed, and widgets)

---
+++ tutorial-clone-site/src/Feed.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed(){
+    return (
+        <div className="feed">
+            {/* Header */}
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+            {/* Tweetbox */}
+            
+            
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+        </div>
+    )
+}
+
+export default Feed
